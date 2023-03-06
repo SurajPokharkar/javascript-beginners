bf6b17a8c4c46fd7f56cd94023aca4bd7bec6e78
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-06 14:26:18 +0000

    First User Signup File

---
+++ 02_intermediate/firstUserSignup.js
@@ -0,0 +1,46 @@
+
+const userID = "$abc123456"
+
+let firstName = "Abcd"
+let lastName = "Xyzpqrs"
+
+let userEmail = "abcd1234@beginner.js"
+let userPassword = "9876543210.js"
+let confirmUserPassword = "9876543210"
+
+let userCountry = "India"
+let userState = "Maharashtra"
+let userCity = "Pune"
+let userPincode = 410503
+
+let isUserLoggedInFromGoogle = false
+let isUserLoggedInFromMeta = false
+let isUserLoggedInFromApple = true
+
+console.log(`
+    USER ID: ${userID}
+    FIRST NAME: ${firstName}
+    LAST NAME: ${lastName}
+    EMAIL: ${userEmail}
+    COUNTRY: ${userCountry}
+    STATE: ${userState}
+    CITY: ${userCity}
+    PINCODE: ${userPincode}
+    LOGGEDIN FROM APPLE: ${isUserLoggedInFromApple}
+
+`
+);
+
+
+// OUTPUT
+
+// USER ID: $abc123456
+// FIRST NAME: Abcd
+// LAST NAME: Xyzpqrs
+// EMAIL: abcd1234@beginner.js
+// COUNTRY: India
+// STATE: Maharashtra
+// CITY: Pune
+// PINCODE: 410503
+// LOGGEDIN FROM APPLE: true
+
