3974028e6efd0c4470ffa90addee5225857489fe
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-06 13:54:27 +0000

    ECMAscript History

---
+++ 01_basics/ECMA.js
@@ -0,0 +1,19 @@
+//  ECMAscript
+
+// ECMA - European Computer Manufacturing Association Script
+
+// For all the browsers to support many different scripting languages it became a tedious task.
+
+// This is where ECMAscript came into the picture.
+
+// It wrote guidelines/standards to follow for all the scripting languages out there.
+
+// Javascript is one these Languages that follow such guidelines.
+
+// ES6 release made a drastic change in the world of Javascript
+// ES6 - 2015
+// ES8 - 2017
+
+// A lot more versions futher came up...
+
+
