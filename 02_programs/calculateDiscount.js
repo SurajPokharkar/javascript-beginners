6dd0042387caf6537811f5c1334671ccb8d8b30e
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-06 15:12:31 +0000

    calulate discount-program file added

---
+++ 02_programs/calculateDiscount.js
@@ -0,0 +1,14 @@
+// Calculate Discount on a Product
+
+
+let sellingPrice = 199
+let listingPrice = 799
+
+// Formula =>  D = ((L - S)/L) * 100
+let discountPercent = ((listingPrice - sellingPrice)/listingPrice) * 100
+
+let displayDiscount = Math.round(discountPercent)
+console.log(displayDiscount);   // 75
+
+
+
