d5dcac7fd64ec44b02db1f80e27490c150d5f620
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-09 11:19:09 +0000

    conditional js

---
+++ 01_basics/08_conditionals.js
@@ -0,0 +1,12 @@
+
+let temperatureOutside = 20  // 20 DEGREE
+
+if (temperatureOutside < 20) {
+     console.log("It's COLD");
+    } 
+else {
+    console.log("It's not so COLD");
+    
+     }
+
+
