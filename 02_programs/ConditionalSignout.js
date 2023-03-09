3b1dc1f5c6010ce91e01ac6f131444e1e98b979f
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-09 13:01:18 +0000

    Conditional Signout program done

---
+++ 02_programs/ConditionalSignout.js
@@ -0,0 +1,13 @@
+let userAuthenticated = true
+
+// if (userAuthenticated){
+//     console.log("Show Signout Button");
+// }
+// else{
+//     console.log("Show Login Button");
+// }
+
+
+// ternary operator =>  (condition ? if : else )
+
+userAuthenticated ? console.log("Show Signout Button") :  console.log("Show Login Button")
