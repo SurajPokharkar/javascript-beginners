65178ac6db41909d4b570b6b045918b0ef63e466
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-12 18:36:02 +0000

    Hoisting and Context Theory of JS done

---
+++ 03_intermediate/04_hoisting.js
@@ -0,0 +1,19 @@
+// There are two types of major Context running/available while executing a program
+// Global Context and Execution Context
+
+// Global Context=> Collects the information about anything and everything. It's on top of Execution Context
+
+// Execution Context=> kicks in when you want to run some code (maybe one line of code or totally a whole function)
+// After execution of code, it goes away.
+
+// Execution Context=> 
+// Variable Object 
+// Scope Chain
+// this 
+
+// 2 Rules to follow whenever there is Execution Context
+// 1]Function Declarations are scanned and made available
+// 2]Variable Declarations are scanned and made undefined
+
+
+
