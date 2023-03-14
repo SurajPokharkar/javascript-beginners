3419c17c784bd338f10e08461578f49ce5b991b3
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-03-14 14:43:02 +0000

    array in JS

---
+++ 03_intermediate/06_array.js
@@ -0,0 +1,19 @@
+// Array
+
+// an array is a special data type that allows you to store a collection of values (elements) of any data type 
+// (such as numbers, strings, objects, etc.) in a single variable.
+
+// An array is defined using square brackets [], with each element separated by a comma.
+
+const myArr = [5, 4, 3, 2, 1, 0, 12, 50]
+console.log(myArr[5]); // outputs 0 (5th element)
+
+// Another way to declare array=>
+
+const myArr2 = new Array(1, 2, 3, 4, 5) // It would automatically add square brackets
+console.log(myArr2[5]);  // undefined
+
+
+
+
+
