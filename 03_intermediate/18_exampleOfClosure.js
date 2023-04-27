65fe96cc8d35d12f6261576dccbba75651a6779b
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-04-27 05:45:21 +0000

    updated comments in the example of closure

--- 03_intermediate/18_exampleOfClosure.js
+++ 03_intermediate/18_exampleOfClosure.js
@@ -8,7 +8,7 @@ function doAddition(x) {
 var addFive = doAddition(10)
 console.log(addFive(5))
 
-// doAddition() never goes off from the memory because still one reference is still alive of it below.
+// doAddition() never goes off from the memory because still one reference is still alive of it => doAddition()()
 
 // This is all possible because of closure and also how memory works in JS
 console.log(doAddition(5)(5))   // Another way to to do it
