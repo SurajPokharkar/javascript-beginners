f79c1a680b19ff26ddc1572d0bc68433037e48cf
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-04-27 12:30:26 +0000

    Added template literals

---
+++ 05_advanced/01_stringLiterals.js
@@ -0,0 +1,9 @@
+
+let name = `Suraj`
+let fullName = `${name} Pokharkar`
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("Suraj"));
+console.log(fullName.toUpperCase());
