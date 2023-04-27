4fca65da2d25664c607774ff46c8ce0306fc3ece
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-04-27 10:38:08 +0000

    modified bind & call

--- 03_intermediate/19_bind&Call.js
+++ 03_intermediate/19_bind&Call.js
@@ -1,16 +1,19 @@
 
+
 const suraj = {
     firstName : "Suraj",
     lastName : "Pokharkar",
     role : "Admin",
     courseCount : 5,
     getInfo : function () {
-        console.log(`
-            Firstname is ${this.firstName} 
-            Lastname is ${this.lastName} 
-            Role is ${this.role} 
-            Studying total ${this.courseCount} Courses
-        `)
+        console.log(
+            `
+             Firstname is ${this.firstName}
+             Lastname is ${this.lastName}
+             Role is ${this.role}
+             Studying total ${this.courseCount} Courses
+            `
+        );
     }
 }
 
@@ -18,8 +21,19 @@ const suraj = {
 const chris = {
     firstName : "Chris",
     lastName : "Pratt",
-    role : "Sub Admin",
-    courseCount : 5
+    role : "Sub-Admin",
+    courseCount : 4
+}
 
 
-suraj.getInfo()
+// bind() => binds the object
+// bind gives you a reference- you need to actually call the function 
+
+// Eg:
+// getInfo - reference to the method 
+// getInfo() - calling the method
+
+suraj.getInfo.bind(chris)()        // bind() - gives you a reference back 
+
+suraj.getInfo.call(chris)           // call() - directly calls the method
+
