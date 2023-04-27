f966016b99fe668f256ae96772fff1c9250a0c3c
Suraj Pokharkar <122016148+SurajPokharkar@users.noreply.github.com>
2023-04-27 05:41:26 +0000

    example of addition in closure added

---
+++ 03_intermediate/18_exampleOfClosure.js
@@ -0,0 +1,17 @@
+
+function doAddition(x) {
+    return function(y){
+        return x+y
+    }
+}
+
+var addFive = doAddition(10)
+console.log(addFive(5))
+
+// doAddition() never goes off from the memory because still one reference is still alive of it below.
+
+// This is all possible because of closure and also how memory works in JS
+console.log(doAddition(5)(5))   // Another way to to do it
+
+
+
